6224cf5ce698f22d76a06deacae3ea5eff274202
Cauan Cabral <cauan@radig.com.br>
2010-04-19 23:33:07 -0400

    Refatoração completa do behavior
    Removido dependências externas
    Agora o behavior pode ser usado por qualquer instalação com PHP >= 5.2.4
    Melhor tratamento e suporte a conversão de datas

--- locale.php
+++ locale.php
@@ -1,97 +1,90 @@
 <?php
-/* 
- * $Id$
- *
+/** 
  * @author Cauan Cabral - cauan@radig.com.br
  *
- * @copyright Cauan Cabral
- * @license MIT License
+ * @copyright 2009-2010, Radig - Soluções em TI, www.radig.com.br
+ * @license MIT
  *
  * @package Radig
  * @subpackage L10n
- * @version $Rev$
  * 
  * Este behavior requer PHP versão >= 5.2.4
- * Utiliza a lib "intl", disponível no PECL - http://pecl.php.net/package/intl
- * 
- * Caso utilize PHP versão >= 5.3, ela já esta builtin (não precisa instalar nada)
  */
+
+App::import('CORE', 'ConnectionManager');
 
 class LocaleBehavior extends ModelBehavior
 {
-	public $name = 'Locale';
+	protected $model;
 	
-	public $model;
+	private $cakeAutomagicFields = array('created', 'updated', 'modified');
 	
-	private $cakeAutomagicFields = array('created', 'updated');
+	private $typesFormat;
 	
-	private $langs = array(
-		'finalLang' => 'en_US',
-		'initLang' => 'en_US'
-		);
+	private $systemLang;
 
-	function setup(&$Model, $config)
+	public function setup(&$model, $config = array())
 	{
-		$this->model = $Model;
+		$this->model = $model;
 		$this->settings = $config;
 		
-		$lang = explode('-', Configure::read('Config.language'));
+		$this->systemLang = Configure::read('Language.default');
 		
-		if(isset($lang[1]))
+		$db =& ConnectionManager::getDataSource($this->model->useDbConfig);
+		
+		foreach($db->columns as $type => $info)
 		{
-			$lang[1] = strtoupper($lang[1]);
+			if(isset($info['format']))
+				$this->typesFormat[$type] = $info['format'];
 		}
-		
-		$lang = implode('_', $lang);
-		
-		//verifica se é um locale "válido" pro PHP (5 caracteres)
-		if(isset($lang[4]))
-		{
-			$this->langs['initLang'] = $lang;
-		}
-		else
-		{
-			$this->langs['initLang'] = Configure::read('Language.default_php');
-		}
-		
-		ini_set('intl.default_locale', $this->langs['initLang']);
 	}
 
-	public function beforeValidate()
+	public function beforeValidate(&$model)
+	{	
+		parent::beforeValidate($model);
+		
+		return $this->localizeData();
+	}
+	
+	public function beforeSave(&$model)
 	{
-		$success = TRUE;
+		parent::beforeSave($model);
+		
+		return $this->localizeData();
+	}
+	
+	protected function localizeData()
+	{
+		$status = TRUE;
 		
 		// verifica se há dados setados no modelo
 		if(isset($this->model->data) && !empty($this->model->data))
 		{
 			// varre os dados setados
-			foreach($this->model->data[$this->model->name] as $field => &$value)
+			foreach($this->model->data[$this->model->name] as $field => $value)
 			{
-				// caso o campo esteja vazio e não tenha um array como valor, e ainda não seja um campo automagico do cake
+				// caso o campo esteja vazio e não tenha um array como valor
 				if(!empty($value) && !is_array($value) && !in_array($field, $this->cakeAutomagicFields))
 				{
 					switch($this->model->_schema[$field]['type'])
 					{
 						case 'date':
-							$success = $this->__dateConvert($value);
+						case 'datetime':
+						case 'time':
+						case 'timestamp':
+							$status = ($status && $this->__dateConvert($this->model->data[$this->model->name][$field], $this->model->_schema[$field]['type']));
 							break;
 						case 'decimal':
-							$success = $this->__decimalConvert($value);
-							break;
 						case 'float':
 						case 'double':
-							$success = $this->__stringToFloat($value);
+							$status = ($status && $this->__stringToFloat($this->model->data[$this->model->name][$field]));
 							break;
 					}
 				}
-				
-				// caso alguma conversão não seja bem sucedida, aborta o tratamento e retorna false
-				if(!$success)
-				{
-					return FALSE;
-				}
 			}
 		}
+		
+		return $status;
 	}
 
 	/**
@@ -102,36 +95,44 @@ class LocaleBehavior extends ModelBehavior
 	 */
 	private function __decimalConvert(&$value)
 	{
-		$this->__stringToFloat($value);
-		
-		$nf = new NumberFormatter($this->langs['finalLang'], NumberFormatter::DECIMAL);
-		
-		if($nf === FALSE)
-		{
-			return FALSE;
-		}
-		
-		$value = $nf->format($value);
-		
-		return ($value !== FALSE);
+		//TODO implementar um método específico para conversão de decimais, sem depender de extensão
 	}
 
 	/**
 	 * Converte uma data localizada para padrão de banco de dados (americano)
 	 * 
 	 * @param string $value
+	 * @param string $type -> a valid schema date type, like: 'date', 'datetime', 'timestamp' or 'time'
 	 * @return bool
 	 */
-	private function __dateConvert(&$value)
-	{		
-		$df = new IntlDateFormatter($this->langs['finalLang'],  IntlDateFormatter::SHORT, IntlDateFormatter::NONE);
+	private function __dateConvert(&$value, $type = 'date')
+	{
+		if($this->systemLang === 'pt-br')
+		{
+			/*
+			 * @FIXME remover redundância de busca de padrão
+			 */
+			if( preg_match('/^\d{1,2}\/\d{1,2}\/\d{2,4}/', $value) )
+			{
+				$value = preg_replace('/^(\d{1,2})\/(\d{1,2})\/(\d{2,4})/', '$3-$2-$1', $value);
+			}
+			else if( preg_match('/^\d{1,2}\-\d{1,2}\-\d{2,4}/', $value) )
+			{
+				$value = preg_replace('/^(\d{1,2})\-(\d{1,2})\-(\d{2,4})/', "$3-$2-$1", $value);
+			}
+			
+			if( $value == null )
+				return FALSE;
+		}
 		
-		if($df === FALSE)
+		$dt = new DateTime($value);
+		
+		if($dt === FALSE)
 		{
 			return FALSE;
 		}
 		
-		$value = $df->format($value);
+		$value = $dt->format($this->typesFormat[$type]);
 		
 		return ($value !== FALSE);
 	}
@@ -150,11 +151,16 @@ class LocaleBehavior extends ModelBehavior
 	private function __stringToFloat(&$value)
 	{
 		if(is_string($value))
-		{
+		{	
 			// find decimal digits
 			if(preg_match('/([\.|,])([0-9]*)$/', $value, $d))
 			{
-			  $d = $d[2];
+				$d = $d[2];
+			}
+			else
+			{
+				// insert zero to rigth side (two for convenience)
+				$d = '00';
 			}
 			
 			// extract integer digits
