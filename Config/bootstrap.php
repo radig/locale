18d3393c27ca54feef49fd4a5df47bec1e404ccd
Cauan Cabral <cauan@radig.com.br>
2013-03-04 17:11:32 -0400

    Using America/Sao_Paulo as default timezone

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -3,7 +3,7 @@
 Configure::write('Language.default', 'pt-br');
 
 /* Definição de timezone para usar a classe DateTime */
-date_default_timezone_set('America/Campo_Grande');
+date_default_timezone_set('America/Sao_Paulo');
 
 /* Definição de locale para formatar saída de Data, Hora, moedas e etc */
 setlocale(LC_ALL, 'pt_BR.utf-8', 'pt_BR', 'pt-br', 'pt_BR.iso-8859-1');
